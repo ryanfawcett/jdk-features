952eee7408eda3acec27f9fccd9f92361473a8a4
ryanfawcett <tangch3ng@mail.com>
2024-03-15 09:48:45 +0800

    feature: JDK8中日期时间API

--- jdk8/src/test/java/org/example/date/LocalDateTimeTest.java
+++ jdk8/src/test/java/org/example/date/LocalDateTimeTest.java
@@ -2,7 +2,13 @@ package org.example.date;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.Period;
+import java.time.ZoneOffset;
 
 @SuppressWarnings("all")
 class LocalDateTimeTest {
@@ -17,5 +23,50 @@ class LocalDateTimeTest {
 
         LocalDateTime ldt3 = ldt.plusYears(2);
         System.out.println(ldt3);
+
+        System.out.println(ldt.getYear());
+        System.out.println(ldt.getMonth());
+        System.out.println(ldt.getDayOfMonth());
+        System.out.println(ldt.getHour());
+        System.out.println(ldt.getMinute());
+        System.out.println(ldt.getSecond());
+    }
+
+    /**
+     * 时间戳
+     */
+    @Test
+    void instant() {
+        Instant now = Instant.now(); // 默认获取UTC时区
+        System.out.println("now = " + now);
+
+        OffsetDateTime offsetDateTime = now.atOffset(ZoneOffset.ofHours(8));
+        System.out.println("offsetDateTime = " + offsetDateTime);
+
+        System.out.println(now.toEpochMilli()); // 获取对应的毫秒值
+    }
+
+    /**
+     * Duration - 计算两个时间之间的间隔
+     * Period - 计算两个日期之间的间隔
+     */
+    @Test
+    void duration() throws InterruptedException {
+        Instant ins1 = Instant.now();
+        Thread.sleep(1000);
+        Instant ins2 = Instant.now();
+
+        // 计算时间戳之间的间隔
+        Duration between = Duration.between(ins1, ins2);
+        System.out.println("between = " + between);
+        System.out.println(between.toMillis());
+
+
+        LocalDate ldt1 = LocalDate.now();
+        Thread.sleep(1000);
+        LocalDate ldt2 = LocalDate.now();
+        Period period = Period.between(ldt1, ldt2);
+
+        System.out.println(period.getDays());
     }
 }
